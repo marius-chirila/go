9e17e75c84fdf918d5cf880099bcc0ba8174ed77
Marius Chirila <marius.chirila.91@gmail.com>
2025-01-18 10:42:43 +0100

    printing values in different modes

--- fmt/main.go
+++ fmt/main.go
@@ -12,6 +12,6 @@ func main() {
 	a, b, c, d, e, f := 0, 1, 2, 3, 4, 5
 	fmt.Printf("Adams as a decimal: %d\n", adams)
 	fmt.Printf("Adams as a hexideciaml: %x \n", adams)
-	fmt.Printf("a,b,c,d,e,f: %x %x %x %x %x %x\n", a, b, c, d, e, f)
+	fmt.Printf("a,b,c,d,e,f: %#x %#x %#x %#x %#x %#x\n", a, b, c, d, e, f)
 	fmt.Printf("a,b,c,d,e,f: %b %b %b %b %b %b\n", a, b, c, d, e, f)
 }
