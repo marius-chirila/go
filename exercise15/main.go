f319cfed8073fd6ab7c6457c8947279657bdb5e7
Marius C <marius.chirila.91@gmail.com>
2025-01-21 12:07:43 +0100

    loops+modulus

--- exercise15/main.go
+++ exercise15/main.go
@@ -5,5 +5,10 @@ import "fmt"
 func main() {
 	x, y := 83/40, 83%40 // have a modulus result of 83 divided by 40 in float
 	fmt.Println(float64(x) + 0.1*float64(y))
+	for i := 0; i < 100; i++ {
+		if i%2 != 0 {
+			fmt.Printf("This is not an even number %d\n", i)
+		}
+	}
 
 }
