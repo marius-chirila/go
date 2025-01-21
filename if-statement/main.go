d63881b6915962122c70b330d919c646f61a18e6
Marius Chirila <marius.chirila.91@gmail.com>
2025-01-21 11:22:54 +0100

    if conditional

---
+++ if-statement/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var s int
+	fmt.Println("Give an int")
+	fmt.Scan(&s)
+	if s < 42 {
+		fmt.Println("Smaller than life!")
+	} else if s == 42 {
+		fmt.Println("The answer to life, the universe and everything!")
+	} else if s > 42 {
+		fmt.Println("Bigger than life!")
+	} else {
+		fmt.Println("Not an intiger")
+	}
+}
