d00ec22f22b1a6557e5e873bc9e399c93f453d13
Marius C <marius.chirila.91@gmail.com>
2025-01-27 15:57:27 +0100

    variadic functions

--- functions/main.go
+++ functions/main.go
@@ -23,9 +23,23 @@ func dogYears(name string, ageDogYears int) (string, int) {
 	return fmt.Sprintf("My name is: %v and I would be in dog years:", name), ageDogYears
 }
 
+func variadicFunc(x ...int) int { //variadic func with variadic parameters "..."
+	fmt.Printf("%v, %T", x, x)
+	sum := 0
+	for _, v := range x {
+		sum += v
+	}
+	return sum
+}
+
 func main() {
+	xi := []int{42, 44, 46}
 	foo()
 	bar("I'm from bar!")
 	fmt.Println(bars("More bars!"))
 	fmt.Println(dogYears("Todd", 33))
+	sum := variadicFunc(42, 33, 54)
+	fmt.Println("Sum is: ", sum)
+	sum2 := variadicFunc(xi...)
+	fmt.Println("New sum is: ", sum2)
 }
